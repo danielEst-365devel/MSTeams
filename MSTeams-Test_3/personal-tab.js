b1ad57652039ec1513bc39458196a650d531fe9e
danielEst-365devel <141566796+danielEst-365devel@users.noreply.github.com>
2023-11-03 12:05:49 +0800

    Update personal-tab.js

--- MSTeams-Test_3/personal-tab.js
+++ MSTeams-Test_3/personal-tab.js
@@ -1,1 +1,22 @@
 microsoftTeams.initialize();
+
+function displayTasks() {
+    // Assuming you have a backend API endpoint to fetch tasks
+    fetch('https://api.example.com/tasks')
+        .then(response => response.json())
+        .then(tasks => {
+            const taskList = document.getElementById('taskList');
+            // Clear existing task list
+            taskList.innerHTML = '';
+
+            // Populate task list with fetched tasks
+            tasks.forEach(task => {
+                const taskItem = document.createElement('div');
+                taskItem.textContent = task.name; // Assuming each task object has a 'name' property
+                taskList.appendChild(taskItem);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching tasks:', error);
+        });
+}
